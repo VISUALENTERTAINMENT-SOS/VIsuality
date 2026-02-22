57b5fb27d5a56a1bd9e8003725988c55166adb93
VISUALENTERTAINMENT-SOS <anugerahsandag476@gmail.com>
2026-02-22 22:19:03 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    const elements = document.querySelectorAll(".animate");
-
-    elements.forEach((el, index) => {
-        setTimeout(() => {
-            el.classList.add("show");
-        }, index * 300);
-    });
-});
